c8672d1a711b68b613f04bb789a1935fbd509056
Max-42 <max@oppermann.fun>
2021-05-22 00:59:12 +0200

    Check if user is using the default file. ☑️

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from sys import executable, path #Um dependencies zu installiern.
 import json #Um die Configs zu lesen
 import os
+import hashlib
 
 
 
@@ -36,32 +37,46 @@ def main():
         logger("Could not find the settings file, creating one for you...")
         config = {"_comment": "Make sure to use correct JSON syntax.", "logging": "True", "protocol": "https://"}
 
-        with open(settingsfile, 'w') as f:
+        with open(settingsfile, 'w', encoding='utf-8') as f:
             json.dump(config, f)
             f.close
+        
+        print("Exiting because if we had to create a settings file for you, the correct settings cannot yet be in there.")
+        exit(1)
 
     #Zugangsdaten Datei anlegen
     if not os.path.isfile(credentialsfile):
         logger("Could not find the credentials file, creating one for you...")
         config = {"_comment": "Make sure to NOT share this file! And please make sure to use correct JSON syntax.","host": "schule.de/iserv", "username": "vorname.nachname", "password":"yOUr-S€cuRe-P4§$w0RD" }
 
-        with open(credentialsfile, 'w') as f:
+        with open(credentialsfile, 'w', encoding='utf-8') as f:
             json.dump(config, f)
             f.close
+        
+        print("Exiting because if we had to create a config for you, the correct credentials cannot yet be in there.")
+        exit(1)
 
     
     if logger():
-        with open(settingsfile, 'r') as f:
+        with open(settingsfile, 'r' ,encoding='utf-8') as f:
             global settings
             settings = json.load(f)
             f.close
         logger("Settings: " + str(settings))
 
-    with open(credentialsfile, 'r') as f:
+    with open(credentialsfile, 'r', encoding='utf-8') as f:
         global credentials
         credentials = json.load(f)
         f.close
     
+    if (hashlib.sha1(str(credentials).encode('utf-8')).hexdigest()) == "a8a15dd72b6e1cddb4f238a64f1b4c87263a33d9":
+        print("You are using the default credentials file please make sure to fill out this file.")
+        exit(1)
+    
+    if(hashlib.sha1(str(settingsfile).encode('utf-8')).hexdigest())== "720e0948396da23020a348be4d5a17dc44829ae1":
+        print("You are using the default settings file please make sure to fill out this file.")
+        exit(1)
+
     request()
     
    
