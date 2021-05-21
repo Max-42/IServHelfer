05dc49dd0fc9233c1b17e863c92798b43c3bd27e
Max-42 <max@oppermann.fun>
2021-05-21 15:43:49 +0200

    Added config support. ⚙️

--- main.py
+++ main.py
@@ -1,13 +1,53 @@
 #!/usr/bin/env python3
 
-from sys import executable #Um dependencies zu installiern.
-
+from sys import executable, path #Um dependencies zu installiern.
 import json #Um die Configs zu lesen
+import os
 
+logging = True
+
+def logger(message = ""):
+    if logging:
+        if message != "":
+            print(str(message))
+        return True
+    return False
 
 def main():
-   pass
 
+    global workingpath
+    workingpath = os.path.dirname(os.path.realpath('__file__')) #getting current path
+    settingsfile = os.path.join(workingpath, 'config' , 'settings.json')
+    credentialsfile = os.path.join(workingpath, 'config' , 'credentials.json')
+    
+    logger("Reading config...")
+
+    # Einstellungs Datei anlegen
+    if not os.path.isfile(settingsfile): 
+        logger("Could not find the settings file, creating one for you...")
+        config = {"_comment": "Make sure to use correct JSON syntax.", "logging": "True", "protocol": "https://"}
+
+        with open(settingsfile, 'w') as f:
+            json.dump(config, f)
+            f.close
+
+    #Zugangsdaten Datei anlegen
+    if not os.path.isfile(credentialsfile):
+        logger("Could not find the credentials file, creating one for you...")
+        config = {"_comment": "Make sure to NOT share this file! And please make sure to use correct JSON syntax.","host": "schule.de/iserv", "username": "vorname.nachname", "password":"yOUr-S€cuRe-P4§$w0RD" }
+
+        with open(credentialsfile, 'w') as f:
+            json.dump(config, f)
+            f.close
+
+    
+    if logger():
+        with open(settingsfile, 'r') as f:
+           settings = json.load(f)
+           f.close
+        logger("Settings: " + str(settings))
+    
+   
 
 
 
